809c4977249f5f9d246e5d06e6f693b109937355
nishant <nishantkudale04@gmail.com>
2026-01-19 01:29:37 +0530

    update schema.sql to redefine users and authorities tables with improved column sizes and constraints

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,39 @@
-create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
-create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
-create unique index ix_auth_username on authorities (username,authority);
+-- create table users(username varchar_ignorecase(50) not null primary key,
+--                    password varchar_ignorecase(500) not null,
+--                    enabled boolean not null);
+-- create table authorities (username varchar_ignorecase(50) not null,
+--                           authority varchar_ignorecase(50) not null,
+--                           constraint fk_authorities_users
+--                               foreign key(username)
+--                                   references users(username));
+-- create unique index ix_auth_username on authorities (username,authority);
+
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE  users(
+    username VARCHAR(100) NOT NULL PRIMARY KEY,
+    password VARCHAR(100) NOT NULL,
+    enabled  BOOLEAN NOT NULL
+);
+
+CREATE TABLE authorities(
+    username VARCHAR(100) NOT NULL,
+    authority VARCHAR(100) NOT NULL,
+    CONSTRAINT fk_authority_users FOREIGN KEY(username) references users(username)
+
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON  authorities (username, authority);
+
+
+
+
+
+
+
+
+
+
+
+
