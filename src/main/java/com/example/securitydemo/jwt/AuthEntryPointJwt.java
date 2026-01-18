313d97016c129642621b9eaf511ecab8fb1c2324
nishant <nishantkudale04@gmail.com>
2026-01-19 01:27:54 +0530

    update application properties for MySQL configuration and enhance logging in AuthEntryPointJwt

--- src/main/java/com/example/securitydemo/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/example/securitydemo/jwt/AuthEntryPointJwt.java
@@ -5,11 +5,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
+
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -30,11 +32,11 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
         final Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("Status",HttpServletResponse.SC_UNAUTHORIZED);
-        responseBody.put("message", authException.getMessage());
+        responseBody.put("error","Unauthorized");
         responseBody.put("message", authException.getMessage());
         responseBody.put("Path", request.getServletPath());
 
-        final ObjectMapper mapper = new ObjectMapper();
+        final ObjectMapper  mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), responseBody);
 
     }
